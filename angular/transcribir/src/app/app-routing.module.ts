4b18aefb62e09ba4531caba93d77965c98c79a23
Aaron O'Brien <aaron.obrien@sunlife.com>
2019-11-07 16:47:38 -0600

    Adding new routes

--- angular/transcribir/src/app/app-routing.module.ts
+++ angular/transcribir/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginWidgetComponent } from "./login-widget/login-widget.component";
 import { RegistrationWidgetComponent } from "./registration-widget/registration-widget.component";
 import { RegistrationConfirmationWidgetComponent } from "./registration-confirmation-widget/registration-confirmation-widget.component";
 import { CaptureAudioWidgetComponent } from './capture-audio-widget/capture-audio-widget.component';
+import { ViewMediaWidgetComponent } from './view-media-widget/view-media-widget.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginWidgetComponent},
   { path: 'registration', component: RegistrationWidgetComponent},
   { path: 'registrationcomplete', component: RegistrationConfirmationWidgetComponent},
-  { path: 'captureaudio', component: CaptureAudioWidgetComponent}
+  { path: 'captureaudio', component: CaptureAudioWidgetComponent},
+  { path: 'viewMedia', component: ViewMediaWidgetComponent}
 
 ];
 
