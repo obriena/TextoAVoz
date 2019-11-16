ec656c6d788949dc3114865feb14ffaeddc32b69
Aaron <aobrien@fs.gmail.com>
2019-11-16 09:20:21 -0600

    base setup workign through SSL problems

--- services/src/main/java/com/flyingspheres/services/application/cde/MongoProducer.java
+++ services/src/main/java/com/flyingspheres/services/application/cde/MongoProducer.java
@@ -1,8 +1,9 @@
 package com.flyingspheres.services.application.cde;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.ServerAddress;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 import javax.annotation.Resource;
@@ -21,13 +22,30 @@ public class MongoProducer {
     @Resource( lookup = "userDataBase", name= "userDataBase")
     String dataBase;
 
+    @Resource( lookup = "mongoConnectString", name = "mongoConnectString")
+    String mongoConnectionString; //mongodb+srv://<userId>:<password>@<server>
+
+    @Resource( lookup = "mongoUserId", name = "mongoUserId")
+    String mongoUserId;
+
+    @Resource( lookup = "mongoPassword", name = "mongoPassword")
+    String mongoPassword;
+
 
     @Produces
     public MongoClient createMongo() {
         System.out.println("Create Mongo Client.");
         System.out.println("Trying to connect with resources: \n\tHost name: " + hostName + "\n\tPort:" + port);
+        String parsedConnection = mongoConnectionString.replace("{userId}", mongoUserId);
+        parsedConnection = parsedConnection.replace("{password}", mongoPassword);
+        parsedConnection = parsedConnection.replace("{server}", hostName);
 
-        return new MongoClient(new ServerAddress(hostName, port.intValue()), new MongoClientOptions.Builder().build());
+        System.out.println("Connection String: " + parsedConnection);
+        ConnectionString conString = new ConnectionString(parsedConnection);
+
+        MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(conString).retryWrites(true).build();
+
+        return MongoClients.create(settings);
     }
 
     @Produces
